813894cb0a00692f58f18ceedab9aed1275b3a01
potykion <potykion@gmail.com>
2021-02-24 22:42:48 +0300

    Update wishlist + li my-1 <3

--- pages/wishlist.vue
+++ pages/wishlist.vue
@@ -7,10 +7,10 @@
       <li>Электрогриль</li>
       <li><a href="https://streamlabs.com/vansamaofficial/merch/1116981">Футболочка</a></li>
       <li><a HREF="https://www.citilink.ru/catalog/computers_and_notebooks/hdd/ssd_in/1134605/">SSD</a></li>
-      <li>Сертик на <a href="https://www.ozon.ru/category/podarochnye-sertifikaty-32060/">Ozon</a></li>
-      <li>Сертик на <a href="https://www.lamoda.ru/giftcertificate/">Lamoda</a></li>
+      <li><a href="https://www.ozon.ru/category/podarochnye-sertifikaty-32060/">Сертик на Ozon</a></li>
+      <li><a href="https://www.lamoda.ru/giftcertificate/">Сертик на Lamoda</a></li>
       <li>Постельное (подушка, одеяло, матрас)</li>
-      <li>Напольные весы</li>
+      <li>Велотренажер (покруче <a href="https://www.ozon.ru/context/detail/id/166261877">этого</a>)</li>
     </ul>
 
   </div>
