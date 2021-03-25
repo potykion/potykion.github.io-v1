ca7a137252330b7f9bb4043c28b9762d96f3c489
potykion <potykion@gmail.com>
2021-03-25 15:05:33 +0300

    Base swiper

--- components/img-row.vue
+++ components/img-row.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="flex space-x-1 overflow-x-auto ">
-    <template v-for="img in images">
-      <template v-if="imgIsObj(img)">
-        <div>
-          <img v-img="{group: imgGroup}" :src="require(`~/assets${img.img}`)" :alt="img.img">
-          <div class="text-sm italic">{{ img.caption }}</div>
-        </div>
-      </template>
-      <template v-else>
-        <img v-img="{group: imgGroup}" :src="require(`~/assets${img}`)" :alt="img">
-      </template>
+  <div v-swiper:mySwiper="swiperOptions">
+    <div class="swiper-wrapper">
 
-    </template>
+      <div class="swiper-slide" v-for="img in images" :key="img">
+        <template v-if="imgIsObj(img)">
+          <div>
+            <img v-img="{group: imgGroup}" :src="require(`~/assets${img.img}`)" :alt="img.img">
+            <div class="text-sm italic">{{ img.caption }}</div>
+          </div>
+        </template>
+        <template v-else>
+          <img v-img="{group: imgGroup}" :src="require(`~/assets${img}`)" :alt="img">
+        </template>
+      </div>
+
+      <div class="swiper-pagination" slot="pagination"></div>
+    </div>
   </div>
 </template>
 
@@ -29,6 +33,12 @@ import {strToHash} from "~/logic/core/utils/hash";
 export default class ImgRow extends Vue {
   @Prop() images!: string[] | { img: string, caption: string }[];
 
+  swiperOptions = {
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    // Some Swiper option/callback...
+  }
 
   get imgGroup() {
     return strToHash(this.images.join());
--- nuxt.config.js
+++ nuxt.config.js
@@ -38,6 +38,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     "~/plugins/v-img.ts",
+    "~/plugins/swiper.ts",
     "~/plugins/vue-clipboard.ts",
   ],
 
