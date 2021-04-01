e2ddaaa0a94ba854a1d4470fa2598cd831a1f4fa
potykion <potykion@gmail.com>
2021-04-01 17:41:53 +0300

    Беседы в парикмахерской

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,7 @@
 
           <nuxt-link to="/changelog" class="nav-link">
             <div class="nav-link-text">
-              v1.3.2
+              v1.3.3
             </div>
           </nuxt-link>
 
