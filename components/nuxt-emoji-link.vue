c82f9b72d59708208cb3b1ed03b03ce8b11ea748
potykion <potykion@gmail.com>
2021-04-04 22:00:37 +0300

    /gh-sort: js libs

---
+++ components/nuxt-emoji-link.vue
@@ -0,0 +1,24 @@
+<template>
+  <a :href="to">
+    <img src="~/assets/nuxt.png" class=" emote-small" alt="nuxt">
+  </a>
+</template>
+
+
+<script lang="ts">
+
+import {
+  Component,
+  Prop,
+  Vue,
+} from "nuxt-property-decorator"
+
+@Component({})
+export default class NuxtEmojiLink extends Vue {
+  @Prop() to!: string;
+
+}
+</script>
+
+<style scoped>
+</style>
