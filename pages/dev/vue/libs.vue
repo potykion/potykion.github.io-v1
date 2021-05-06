e518dcaaa998729c5293c18a05cd2f4ff522deac
potykion <potykion@gmail.com>
2021-05-06 22:47:07 +0300

    Vue libs & index md rewrite

--- pages/dev/vue/libs.vue
+++ pages/dev/vue/libs.vue
@@ -1,177 +1,56 @@
-<template>
-  <article>
+<!--      @formatter:off -->
 
-    <h2>Vue / Любимые либы</h2>
+<template lang="md">
+<article>
 
-    <h3>UI</h3>
+## Vue / Любимые либы
 
-    <ul>
-      <li>
-        <a href="https://github.com/vuetifyjs/vuetify">vuetify</a>
-        <nuxt-emoji-link to="https://github.com/nuxt-community/vuetify-module"/>
+### UI
 
-        - лучший ui фреймворк про материал дезигн, все
-        что надо есть: сетка, дейтпикеры, селекты и тд
-      </li>
-      <li>
-        <a href="https://github.com/tailwindlabs/tailwindcss">tailwindcss</a>
-        <nuxt-emoji-link
-          to="https://github.com/nuxt-community/tailwindcss-module"/>
+- [vuetify](https://github.com/vuetifyjs/vuetify) <nuxt-emoji-link to="https://github.com/nuxt-community/vuetify-module"/> - лучший ui фреймворк про материал дезигн, все что надо есть: сетка, дейтпикеры, селекты и тд
+- [tailwindcss](https://github.com/tailwindlabs/tailwindcss) <nuxt-emoji-link to="https://github.com/nuxt-community/tailwindcss-module"/> - лучший css фреймворк, если не разрешили пользоваться vuetify
+- [vuejs-datepicker](https://github.com/charliekassel/vuejs-datepicker) - нормальный дейтпикер; альтернативы: [vue-flatpickr-component](https://github.com/ankurk91/vue-flatpickr-component)
+- [vue-multiselect](https://github.com/shentao/vue-multiselect) - лучший селект
+- [nprogress](https://github.com/rstacruz/nprogress) - прогрессбар-полосочка; в Nuxt есть из коробки
+- [Vue.Draggable](https://github.com/SortableJS/Vue.Draggable) - нормальный драг-н-дроп
+- [v-viewer](https://github.com/mirari/v-viewer) - крутой просмотрщик картинок
+- [vue-awesome-swiper](https://github.com/surmon-china/vue-awesome-swiper) - крутой свайпер
+- [vue-yandex-map](https://github.com/PNKBizz/vue-yandex-map) - яндекс карточки
+- [vue-the-mask](https://github.com/vuejs-tips/vue-the-mask) - маски для инпутов, напр. для телефонных номеров
+- [highlightjs](https://highlightjs.org/) <nuxt-emoji-link to="https://www.npmjs.com/package/nuxt-highlightjs"/> - подсветка кода
+- [clipboardjs](https://clipboardjs.com/) <nuxt-emoji-link to="https://www.npmjs.com/package/vue-clipboard2" :is-vue="true"/> - копирование текста
+- [rough-notation](https://github.com/rough-stuff/rough-notation) - стилизовачка текста
+- [spark-joy](https://github.com/sw-yx/spark-joy) - подборочка дезигн приколюх
 
-        - лучший css фреймворк, если не
-        разрешили пользоваться vuetify
-      </li>
-      <li>
-        <a href="https://github.com/charliekassel/vuejs-datepicker">vuejs-datepicker</a> - нормальный дейтпикер;
-        альтернативы: <a href="https://github.com/ankurk91/vue-flatpickr-component">vue-flatpickr-component</a>
-      </li>
-      <li>
-        <a href="https://github.com/shentao/vue-multiselect">vue-multiselect</a> - лучший селект
-      </li>
-      <li>
-        <a href="https://github.com/rstacruz/nprogress">nprogress</a> - прогрессбар-полосочка; в Nuxt есть из коробки
-      </li>
-      <li>
-        <a href="https://github.com/SortableJS/Vue.Draggable">Vue.Draggable</a> - нормальный драг-н-дроп
-      </li>
-      <li>
-        <a href="https://github.com/mirari/v-viewer">v-viewer</a> - крутой просмотрщик
-        картинок
-      </li>
-      <li>
-        <a
-          href="https://github.com/surmon-china/vue-awesome-swiper">vue-awesome-swiper</a>
-        - крутой свайпер
-      </li>
-      <li>
-        <a href="https://github.com/PNKBizz/vue-yandex-map">vue-yandex-map</a> - яндекс карточки
-      </li>
-      <li>
-        <a href="https://github.com/vuejs-tips/vue-the-mask">vue-the-mask</a> - маски для инпутов, напр. для
-        телефонных номеров
-      </li>
+### Тестирование
 
-      <li>
-        <a href="https://highlightjs.org/">highlightjs</a>
-        <nuxt-emoji-link to="https://www.npmjs.com/package/nuxt-highlightjs"/>
-        - подсветка кода
-      </li>
+- [jest](https://github.com/facebook/jest) - лучший фреймворк для юнит и интегрейшн тестов
+- [cypress](https://github.com/cypress-io/cypress) - аналог Selenium - позволяет писать ui тесты для джс + куча плюшек, типа моканья хттп-запросов
+- [husky](https://github.com/typicode/husky) - коммит хуки
 
-      <li>
-        <a href="https://clipboardjs.com/">clipboardjs</a>
-        <nuxt-emoji-link to="https://www.npmjs.com/package/vue-clipboard2" :is-vue="true"/>
-        - копирование текста
-      </li>
+### TS + Vue/Nuxt
 
-      <li>
-        <a href="https://github.com/rough-stuff/rough-notation">rough-notation</a> - стилизовачка текста
-      </li>
+- [vue-class-component](https://github.com/vuejs/vue-class-component), [vue-property-decorator](https://github.com/kaorun343/vue-property-decorator) <nuxt-emoji-link to="https://github.com/nuxt-community/nuxt-property-decorator"/> - позволяет писать vue-компоненты в виде тс-классов
+- [vuex-simple](https://github.com/sascha245/vuex-simple)  - работа с vuex на классах - код читаемее + стейт типизирован - не надо писать <code>mapState</code>, <code>mapGetters</code> и проч.
+- [vue-typedi](https://github.com/sascha245/vue-typedi) - диайчик, плотно интегрируется с либой выше
 
-      <li>
-        <a href="https://github.com/sw-yx/spark-joy">spark-joy</a> - подборочка дезигн приколюх
-      </li>
+### Прочее
 
+- [nvm](https://github.com/nvm-sh/nvm) ([nvm-windows](https://github.com/coreybutler/nvm-windows) для винды) - Node Version Manager - позволяет ставить и менеджерить несколько версий ноды. Полезно, когда работаешь с легаси, напр. с gulp 3, который <a href="https://github.com/gulpjs/gulp/issues/2324">не робит с нодой 12+</a> => легче поставить себе ноду 10.
+- [axios           ](https://github.com/axios/axios) <nuxt-emoji-link to="https://github.com/nuxt-community/axios-module"/> - лучшая либа для http запросов
+- [dayjs           ](https://github.com/iamkun/dayjs) - лучшая либа для работы с датами
+- [ts-debounce     ](https://github.com/chodorowicz/ts-debounce) - рабочий дебаунс для тс
+- [markdown-it     ](https://github.com/markdown-it/markdown-it) <nuxt-emoji-link to="https://github.com/nuxt-community/markdownit-module"></nuxt-emoji-link> - лучший рендер маркдауна
+- [uuid            ](https://github.com/uuidjs/uuid) - ююидики
+- [lodash          ](https://github.com/lodash/lodash) - утилитки
+- [humps           ](https://github.com/domchristie/humps) - конвертит данные из одного нейминга в другой, напр. из camelCase в under_score
+- [ramda           ](https://github.com/ramda/ramda) - фп утилитки
+- [json-server     ](https://github.com/typicode/json-server) - превращает json файл в сервер! - оч удобно для моков
+- [responsively-app](https://github.com/responsively-org/responsively-app) - просмотрщик сайта на браузерах разного размера
 
-    </ul>
-
-    <h3>Тестирование</h3>
-
-    <ul>
-      <li>
-        <a href="https://github.com/facebook/jest">jest</a> - лучший фреймворк для юнит и интегрейшн тестов
-      </li>
-
-      <li>
-        <a href="https://github.com/cypress-io/cypress">cypress</a> - аналог Selenium - позволяет писать ui тесты для
-        джс + куча плюшек, типа моканья хттп-запросов
-      </li>
-
-      <li>
-        <a href="https://github.com/typicode/husky">husky</a> - коммит хуки
-      </li>
-    </ul>
-
-    <h3>TS + Vue/Nuxt</h3>
-
-    <ul>
-      <li>
-        <a href="https://github.com/vuejs/vue-class-component">vue-class-component</a>, <a
-        href="https://github.com/kaorun343/vue-property-decorator">vue-property-decorator</a>
-        <nuxt-emoji-link to="https://github.com/nuxt-community/nuxt-property-decorator"/>
-        - позволяет писать
-        vue-компоненты в виде тс-классов
-      </li>
-
-      <li>
-        <a href="https://github.com/sascha245/vuex-simple">vuex-simple</a> - работа с vuex на классах - код читаемее +
-        стейт типизирован - не надо писать <code>mapState</code>, <code>mapGetters</code> и проч.
-      </li>
-
-      <li>
-        <a href="https://github.com/sascha245/vue-typedi">vue-typedi</a> - диайчик, плотно интегрируется с либой выше
-      </li>
-    </ul>
-
-
-    <h3>Прочее</h3>
-
-    <ul>
-      <li>
-        <a href="https://github.com/nvm-sh/nvm">nvm</a> (<a href="https://github.com/coreybutler/nvm-windows">nvm-windows</a>
-        для винды) - Node Version Manager - позволяет ставить и менеджерить несколько версий ноды. Полезно, когда
-        работаешь с легаси, напр. с gulp 3, который <a href="https://github.com/gulpjs/gulp/issues/2324">не робит с
-        нодой 12+</a> => легче поставить себе ноду 10.
-      </li>
-      <li>
-        <a href="https://github.com/axios/axios">axios</a>
-        <nuxt-emoji-link to="https://github.com/nuxt-community/axios-module"/>
-        - лучшая либа для http запросов
-      </li>
-      <li>
-        <a href="https://github.com/iamkun/dayjs">dayjs</a> - лучшая либа для работы с датами
-      </li>
-
-      <li>
-        <a href="https://github.com/chodorowicz/ts-debounce">ts-debounce</a> - рабочий дебаунс для тс
-      </li>
-
-      <li>
-        <a href="https://github.com/markdown-it/markdown-it">markdown-it</a>
-        <nuxt-emoji-link to="https://github.com/nuxt-community/markdownit-module"></nuxt-emoji-link>
-
-        - лучший рендер маркдауна
-      </li>
-
-      <li>
-        <a href="https://github.com/uuidjs/uuid">uuid</a> - ююидики
-      </li>
-      <li>
-        <a href="https://github.com/lodash/lodash">lodash</a> - утилитки
-      </li>
-
-      <li>
-        <a href="https://github.com/domchristie/humps">humps</a> - конвертит данные из одного нейминга в другой, напр.
-        из camelCase в under_score
-      </li>
-      <li>
-        <a href="https://github.com/ramda/ramda">ramda</a> - фп утилитки
-      </li>
-
-      <li>
-        <a href="https://github.com/typicode/json-server">json-server</a> - превращает json файл в сервер! - оч удобно
-        для моков
-      </li>
-
-      <li>
-        <a href="https://github.com/responsively-org/responsively-app">responsively-app</a> - просмотрщик сайта на
-        браузерах разного размера
-      </li>
-
-
-    </ul>
-
-  </article>
+</article>
 </template>
+<!--      @formatter:on -->
 
 
 <script lang="ts">
--- pages/index.vue
+++ pages/index.vue
@@ -1,102 +1,77 @@
-<template>
+<!--
+todo перенести в how.vue
+Чвич эмоуты тут
+https://www.streamscheme.com/resources/twitch-emotes-meaning-complete-list-monkas-pogchamp-omegalul-kappa/
+-->
 
-  <div>
+<!--      @formatter:off -->
 
-    <p>
-      Привет, я
-      <nuxt-link to="/me">Никита</nuxt-link>
-      и ето мой бложек.
-    </p>
+<template lang="md">
 
-    <p>Здесь будут кулстори, заметки про разработку и экспериенс жизненный.</p>
+Привет, я [Никита](/me) и ето мой бложек.
 
-    <p>И еще здесь может встречаться <b>мат</b> и <b>звенящая пошлость</b>.</p>
+Здесь будут кулстори, заметки про разработку и экспериенс жизненный.
 
-    <p>
-      Что нового в блоге, можно посмотреть в
-      <nuxt-link to="/changelog">ченжлоге</nuxt-link>
-      .
-    </p>
+И еще здесь может встречаться <b>мат</b> и <b>звенящая пошлость</b>.
 
-    <!--      @formatter:off -->
-    <h3>
-      Кулстори
-      <!--
-      todo перенести в how.vue
-      Чвич эмоуты тут
-      https://www.streamscheme.com/resources/twitch-emotes-meaning-complete-list-monkas-pogchamp-omegalul-kappa/
-      -->
-      <img src="~/assets/coolstorybob.png.webp" class="emote" alt="coolstorybob">
-    </h3>
-    <ul>
-      <li><nuxt-link to="/cool-story/fruit-ninja">Фрут Ниндзя</nuxt-link></li>
-      <li><nuxt-link to="/cool-story/pure">Pure</nuxt-link></li>
-      <li><nuxt-link to="/cool-story/small">Свалка маленьких кулстори</nuxt-link></li>
-    </ul>
+Что нового в блоге, можно посмотреть в [ченжлоге](/changelog).
 
-      <h4 class="inline-block">Кукинг</h4>
-      <ul>
-        <li><nuxt-link to="/cool-story/cooking/gourmet">Мечта гурмана</nuxt-link></li>
-        <li><nuxt-link to="/cool-story/cooking/apps">Про приложухи</nuxt-link></li>
-        <li><nuxt-link to="/cool-story/cooking/fast">Готовка по фасту</nuxt-link></li>
-        <li><nuxt-link to="/cool-story/cooking/air-cooker">Аэрогриль</nuxt-link></li>
-        <li><nuxt-link to="/cool-story/cooking/rack-of-lamb">Баранья корейка</nuxt-link></li>
-        <li><nuxt-link to="/cool-story/cooking/my-duck-vision">Утка и рукав</nuxt-link></li>
-        <li><nuxt-link to="/cool-story/cooking/jamie-oliver">Джейми Оливер</nuxt-link></li>
-        <li><nuxt-link to="/cool-story/cooking/other">Малютки</nuxt-link></li>
-      </ul>
+### Кулстори <img src="~/assets/coolstorybob.png.webp" class="emote" alt="coolstorybob">
 
-    <h3>
-      Экспериенс жизненный
-      <img src="~/assets/5Head-Emote.png.webp" class="emote" alt="5Head-Emote">
-    </h3>
-    <ul>
-      <li><nuxt-link to="/wishlist">Вишлист</nuxt-link></li>
-      <li><nuxt-link to="/art">Рисовач</nuxt-link></li>
-      <li><nuxt-link to="/yt">Ютуб</nuxt-link></li>
-      <li><nuxt-link to="/goals2021">Цели 2021</nuxt-link></li>
-      <li><nuxt-link to="/money">Бабос</nuxt-link></li>
-      <li><nuxt-link to="/dance">Денс</nuxt-link></li>
-      <li><nuxt-link to="/gigs">Гиги</nuxt-link></li>
-      <li><nuxt-link to="/nothing-to-do">Нечего делать</nuxt-link></li>
-      <li><nuxt-link to="/sleep">Слипинг</nuxt-link></li>
-    </ul>
+- [Свалка маленьких кулстори](/cool-story/small)
+- [Pure](/cool-story/pure)
+- [Фрут Ниндзя](/cool-story/fruit-ninja)
 
-    <h3>
-      Заметки про разработку
-      <img src="~/assets/Vohiyo.png.webp" class="emote" alt="Vohiyo">
-    </h3>
-    <ul>
-      <li><nuxt-link to="/dev/how">Как блог делается</nuxt-link></li>
-      <li><nuxt-link to="/dev/base">Общие штучки для вебчика</nuxt-link></li>
-      <li><nuxt-link to="/dev/ideas">Идеи для проектов</nuxt-link></li>
+#### Кукинг
 
-    </ul>
+- [Мечта гурмана](/cool-story/cooking/gourmet)
+- [Про приложухи](/cool-story/cooking/apps)
+- [Готовка по фасту](/cool-story/cooking/fast)
+- [Аэрогриль](/cool-story/cooking/air-cooker)
+- [Баранья корейка](/cool-story/cooking/rack-of-lamb)
+- [Утка и рукав](/cool-story/cooking/my-duck-vision)
+- [Джейми Оливер](/cool-story/cooking/jamie-oliver)
+- [Малютки](/cool-story/cooking/other)
 
-    <h4>Python</h4>
-    <ul>
-      <li><nuxt-link to="/dev/python/tests">Тесты</nuxt-link></li>
-      <li><nuxt-link to="/dev/python/sql">SQL</nuxt-link></li>
-      <li><nuxt-link to="/dev/python/libs">Любимые либы</nuxt-link></li>
-    </ul>
+### Экспериенс жизненный <img src="~/assets/5Head-Emote.png.webp" class="emote" alt="5Head-Emote">
 
-    <h4>Flutter</h4>
-    <ul>
-      <li><nuxt-link to="/dev/flutter/firebase">Firebase</nuxt-link></li>
-      <li><nuxt-link to="/dev/flutter/codegen">Кодогенерация</nuxt-link></li>
-      <li><nuxt-link to="/dev/flutter/libs">Любимые либы</nuxt-link></li>
-      <li><nuxt-link to="/dev/flutter/faq">Штуки, которые часто забываются</nuxt-link></li>
-      <li><nuxt-link to="/dev/flutter/release">Релиз приложух в гугл-плее</nuxt-link></li>
-    </ul>
+- [Вишлист](/wishlist)
+- [Рисовач](/art)
+- [Ютуб](/yt)
+- [Цели 2021](/goals2021)
+- [Бабос](/money)
+- [Денс](/dance)
+- [Гиги](/gigs)
+- [Нечего делать](/nothing-to-do)
+- [Слипинг](/sleep)
 
-    <h4>Vue</h4>
-    <ul>
-      <li><nuxt-link to="/dev/vue/libs">Любимые либы</nuxt-link></li>
-    </ul>
-    <!--      @formatter:off -->
+### Заметки про разработку <img src="~/assets/Vohiyo.png.webp" class="emote" alt="Vohiyo">
 
-  </div>
+- [Как блог делается](/dev/how)
+- [Общие штучки для вебчика](/dev/base)
+- [Идеи для проектов](/dev/ideas)
+
+#### Python
+
+- [Тесты](/dev/python/tests)
+- [SQL](/dev/python/sql)
+- [Любимые либы](/dev/python/libs)
+
+#### Flutter
+
+- [Firebase](/dev/flutter/firebase)
+- [Кодогенерация](/dev/flutter/codegen)
+- [Любимые либы](/dev/flutter/libs)
+- [Штуки, которые часто забываются](/dev/flutter/faq)
+- [Релиз приложух в гугл-плее](/dev/flutter/release)
+
+#### Vue
+
+- [Любимые либы](/dev/vue/libs)
+
 </template>
+
+<!--      @formatter:off -->
 
 <script lang="ts">
 
