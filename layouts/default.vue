50aa4e4161c834b8ea72969e60ea6cf0489b6488
potykion <potykion@gmail.com>
2021-06-05 14:29:08 +0300

    Sleeping 2

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,7 @@
 
           <nuxt-link to="/changelog" class="nav-link">
             <div class="nav-link-text">
-              v1.7.2
+              v1.8.0
             </div>
           </nuxt-link>
 
