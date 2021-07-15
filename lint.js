4cf83fb2ad8251c45bdda18112551ce98252d0e0
potykion <potykion@gmail.com>
2021-07-15 17:31:33 +0300

    Lint title in md metadata

---
+++ lint.js
@@ -0,0 +1,29 @@
+/**
+ * Скрипт чекает, что все md-файлы имеют метадату с title
+ */
+
+const glob = require("glob");
+const MarkedMetaData = require("marked-metadata");
+
+// Ищем все md-файлы
+glob("./content/**/**.md", {}, function (er, files) {
+  files.forEach(
+    f => {
+      let metadata;
+      try {
+        // Читаем и парсим md-файл на предмет метадаты
+        const md = new MarkedMetaData(f);
+        md.defineTokens('---', '---');
+        metadata = md.metadata();
+      } catch (e) {
+        throw `В файле нет блока с метадатой:\n${f}`;
+      }
+
+      // Чекаем есть ли title
+      if (!metadata.title) {
+        throw `Не определен title в файле:\n${f}`;
+      }
+    }
+  )
+});
+
