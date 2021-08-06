5ebfff8b5e0251fbfa356dd22b37a291786ef793
potykion <potykion@gmail.com>
2021-08-06 18:11:35 +0300

    Carousel for breakfast and side dish

---
+++ components/cooking/entity.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="rounded shadow bg-white w-48 h-64 flex hover:bg-pink-200 align-middle justify-center px-2 pt-4 cursor-pointer flex-shrink-0">
+    <div class="grid grid-rows-4">
+      <img class="object-scale-down row-span-3 h-full p-2"  :src="img" :alt="title">
+      <div class="row-span-1 m-auto">
+        <div class="text-center text-current">{{ title }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+import {
+  Component,
+  Prop,
+  Vue,
+} from "nuxt-property-decorator"
+
+@Component({
+})
+export default class entity extends Vue {
+  @Prop() title!: string;
+  @Prop() img!: string;
+
+}
+</script>
+
+<style scoped>
+</style>
--- pages/cooking/index.vue
+++ pages/cooking/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <nuxt-content :document="page"/>
+  <div>
+    <article-heading title="Еда" description='Пробую решить проблему "бля что поесть"'></article-heading>
+
+    <h2>Завтраки</h2>
+    <div class="flex space-x-2 overflow-auto">
+      <cooking-entity title="Творог" img="/images/food/cottage-cheese.png"></cooking-entity>
+      <cooking-entity title="Сухие завтраки" img="/images/food/cereals.png"></cooking-entity>
+      <cooking-entity title="Бутерброды" img="/images/food/sandwich.jpg"></cooking-entity>
+    </div>
+
+    <h2>Гарниры</h2>
+    <div class="flex space-x-2 overflow-auto">
+      <cooking-entity title="Рис" img="/images/food/rice.png"></cooking-entity>
+      <cooking-entity title="Гречка" img="/images/food/grechka.png"></cooking-entity>
+      <cooking-entity title="Картошка" img="/images/food/potato.png"></cooking-entity>
+    </div>
+
+  </div>
 
 </template>
 
@@ -12,7 +29,10 @@ export default {
     return {page};
   },
   head() {
-    return generateSeoHead(this.page.title, this.page.description);
+    return generateSeoHead(
+      "Еда",
+      'Пробую решить проблему "бля что поесть"'
+    );
   },
 }
 </script>
