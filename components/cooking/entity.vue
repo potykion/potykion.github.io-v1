9ecc4f4671d266db9580741ffafe738914143c19
potykion <potykion@gmail.com>
2021-08-09 15:26:45 +0300

    Compress food images & food page wip

--- components/cooking/entity.vue
+++ components/cooking/entity.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="rounded shadow bg-white w-48 h-64 flex hover:bg-pink-200 align-middle justify-center px-2 pt-4 cursor-pointer flex-shrink-0">
+  <div class="rounded shadow bg-white w-44 h-60 flex hover:bg-pink-200 align-middle justify-center px-2 pt-4 cursor-pointer flex-shrink-0">
     <div class="grid grid-rows-4">
-      <img class="object-scale-down row-span-3 h-full p-2"  :src="img" :alt="title">
+      <img class="object-scale-down row-span-3 h-full p-5"  :src="img" :alt="title">
       <div class="row-span-1 m-auto">
         <div class="text-center text-current">{{ title }}</div>
       </div>
--- pages/cooking/index.vue
+++ pages/cooking/index.vue
@@ -6,7 +6,7 @@
     <div class="flex space-x-2 overflow-auto">
       <cooking-entity title="Творог" img="/images/food/cottage-cheese.png"></cooking-entity>
       <cooking-entity title="Сухие завтраки" img="/images/food/cereals.png"></cooking-entity>
-      <cooking-entity title="Бутерброды" img="/images/food/sandwich.jpg"></cooking-entity>
+      <cooking-entity title="Бутерброды" img="/images/food/sandwich.png"></cooking-entity>
     </div>
 
     <h2>Гарниры</h2>
@@ -15,6 +15,19 @@
       <cooking-entity title="Гречка" img="/images/food/grechka.png"></cooking-entity>
       <cooking-entity title="Картошка" img="/images/food/potato.png"></cooking-entity>
     </div>
+
+    <h2>Основное блюдо</h2>
+    <div class="flex space-x-2 overflow-auto">
+      <cooking-entity title="Стейк" img="/images/food/steak.png"></cooking-entity>
+      <cooking-entity title="Рыба" img="/images/food/fish.png"></cooking-entity>
+    </div>
+
+    <h2>Салаты</h2>
+    <div class="flex space-x-2 overflow-auto">
+      <cooking-entity title="Помидоры с огурцами" img="/images/food/pomidory-ogurcy.png"></cooking-entity>
+      <cooking-entity title="Винегрет" img="/images/food/vinegret.png"></cooking-entity>
+    </div>
+
 
   </div>
 
