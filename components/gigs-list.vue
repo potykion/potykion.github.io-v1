e1208620a2ebc76e0b4141e38a2187aa993c3257
potykion <potykion@gmail.com>
2021-09-04 21:24:58 +0300

    gigs.vue > gigs.md

---
+++ components/gigs-list.vue
@@ -0,0 +1,59 @@
+<template>
+  <ul>
+    <li v-for="c in concerts" :key="c.title">
+      <a :href="c.link"><b>{{ c.title }}</b> · {{ c.date.toLocaleDateString('ru-RU') }} · {{ c.club }}</a>
+    </li>
+  </ul>
+</template>
+
+
+<script lang="ts">
+
+import {
+  Component,
+  Prop,
+  Vue,
+} from "nuxt-property-decorator"
+
+
+type Club = "Главclub" | "Город";
+
+interface Concert {
+  title: string;
+  link: string;
+  club: Club;
+  date: Date;
+}
+
+@Component({
+})
+export default class GigsList
+extends
+Vue
+{
+  concerts: Concert[] = [
+    {
+      title: "GusGus",
+      link: "https://afisha.yandex.ru/moscow/concert/gusgus-msk",
+      date: new Date("2021-10-15"),
+      club: "Главclub" as Club,
+    },
+    {
+      title: "Perturbator",
+      link: "https://vk.com/perturbator_msk2021",
+      club: "Главclub" as Club,
+      date: new Date("2021-11-26"),
+    },
+    {
+      title: "Ghostemane",
+      link: "https://afisha.yandex.ru/moscow/concert/ghostemane-msk-2021",
+      club: "Главclub" as Club,
+      date: new Date("2021-09-29"),
+    },
+
+  ].sort((a, b) => a.date.getTime() - b.date.getTime());
+}
+</script>
+
+<style scoped>
+</style>
---
+++ content/archive/gigs.md
@@ -0,0 +1,19 @@
+---
+title: Гиги
+description: Концертики, которые я планирую посетить в этом году (если конечно они состоятся в такие ковидные времена)
+createdAt: 2021-06-18
+cover: /images/exp/gigs/ovsi.jpg
+big: true
+---
+
+
+Здесь будет список конциков, на которые я хочу сходить в этому году, если ковид конечно закончится.
+
+<gigs-list></gigs-list>
+
+## Афиши
+
+- [Яндекс Афиша](https://afisha.yandex.ru/moscow/concert)
+- [MADSTREAM BOOKING AGENCY](https://vk.com/madstreambooking)
+- [GRAINS OF SAND BOOKING](https://vk.com/gosbooking)
+- [Афиша Москва | Концерты вечеринки](https://vk.com/msc_events)
